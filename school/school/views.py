71b15b9c30f4a2dcab57aab57b25ae13dbc54c4c
rupadevi664 <rupadevigodi@gmail.com>
2026-07-21 00:14:12 +0530

    first commit

---
+++ school/school/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+def landing(request):
+    return render(request,"index.html")
---
+++ school/teachers/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+# pyrefly: ignore [missing-import]
+from .models import Teacher
+# Register your models here.
+admin.site.register(Teacher)
